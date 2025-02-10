dd3b83b62b42423cd17b684bc006a08b0cc5654b
satinder36 <ssatinder1996@gmail.com>
2025-02-11 00:58:48 +0530

    routes | middlewares | auth intro

--- src/app.js
+++ src/app.js
@@ -1,14 +1,55 @@
 const express = require("express");
 
 const app = express();
+const { adminAuth, userAuth } = require("./middlewares/auth");
 
-app.use("/test", (req, res) => {
-  res.send(" testing");
+app.use("/admin", adminAuth);
+
+app.get("/admin/getAllUsers", (req, res) => {
+  res.send(" all user");
 });
 
-app.use("/", (req, res) => {
-  res.send(" Hello parent");
+app.delete("/admin/deleteUser", (req, res) => {
+  res.send("delete user");
 });
+
+app.get("/user", userAuth, (req, res) => {
+  res.send(" usere pathsss");
+});
+
+//we can wrap the request handlers in an array, its optional though
+app.get("/check", [
+  (req, res, next) => {
+    console.log("hello");
+    // res.send(" first res");
+    next();
+  },
+  (req, res, next) => {
+    res.send("res 2");
+    console.log(" hello 2222");
+    next();
+    console.log("aftr next");
+  },
+]);
+// can get the params with :userId, use ? after :name if the name is optional, use req.query if the path?userId=100
+// app.get("/user/:userId/:name?", (req, res) => {
+//   console.log("params", req.params);
+//   console.log("query", req.query);
+
+//   res.send("get user");
+// });
+
+// app.post("/user", (req, res) => {
+//   res.send("post user");
+// });
+
+// app.use("/test", (req, res) => {
+//   res.send(" testing");
+// });
+
+// app.use("/", (req, res) => {
+//   res.send(" Hello parent");
+// });
 
 app.listen(3000, () => {
   console.log("listening"); //callback func is optional
---
+++ src/middlewares/auth.js
@@ -0,0 +1,20 @@
+const adminAuth = (res, req, next) => {
+  const token = "abc";
+  if (token == "abc") {
+    console.log("ifff");
+    next();
+  } else {
+    res.status(401).send("Un-Auth");
+  }
+};
+
+const userAuth = (req, res, next) => {
+  const token = "abc";
+  if (token == "abc") {
+    next();
+  } else {
+    res.status(401).send("Un-Authhhh");
+  }
+};
+
+module.exports = { adminAuth, userAuth };
