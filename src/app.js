af54b7d52bc55883780d8543aa26a920c138e7cc
satinder36 <ssatinder1996@gmail.com>
2025-02-12 01:13:28 +0530

    update | delete  API

--- src/app.js
+++ src/app.js
@@ -16,6 +16,59 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const userData = await User.find({ emailId: userEmail });
+    if (userData.length !== 0) {
+      res.send(userData);
+    } else {
+      res.status(404).send("Data not found");
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong" + err.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong" + err.message);
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.id;
+  try {
+    const data = await User.findByIdAndDelete(userId); //User.findOneAndDelete({_id:userId})
+    console.log("delete data", data);
+    if (data) {
+      res.send("User deleted successfully");
+    } else {
+      res.send("User with this id not found");
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong" + err.message);
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  // returnDocument: "after" , default is "before", it returns the update value either before
+  // or after based on the returnDocument set value,its optional param
+  try {
+    const dataUpdate = await User.findByIdAndUpdate(userId, data, {
+      returnDocument: "after",
+    });
+    res.send(dataUpdate);
+  } catch (err) {
+    res.status(400).send("Something went wrong" + err.message);
+  }
+});
+
 connectDb()
   .then(() => {
     console.log("connected");
