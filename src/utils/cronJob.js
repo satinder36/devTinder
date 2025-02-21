2281ff4e385f706d31a2f846fd15e30efc80b6c4
satinder36 <ssatinder1996@gmail.com>
2025-02-21 17:55:58 +0530

    cron job added for emails

---
+++ src/utils/cronJob.js
@@ -0,0 +1,41 @@
+const cron = require("node-cron");
+const sendEmail = require("./sendEmail");
+const ConnectionRequestModel = require("../models/connectionRequest");
+const { subDays, startOfDay, endOfDay } = require("date-fns");
+
+cron.schedule("* 8 * * *", async () => {
+  try {
+    const yesterday = subDays(new Date(), 1);
+
+    const yesterdayStart = startOfDay(yesterday);
+    const yesterdayEnd = endOfDay(yesterday);
+
+    const pendingRequests = await ConnectionRequestModel.find({
+      status: "interested",
+      createdAt: {
+        $gte: yesterdayStart,
+        $lt: yesterdayEnd,
+      },
+    }).populate("toUserId", "emailId");
+
+    console.log("pendingRequests ", pendingRequests);
+
+    const listOfEmails = [
+      ...new Set(pendingRequests.map((row) => row?.toUserId?.emailId)),
+    ];
+
+    for (const email of listOfEmails) {
+      try {
+        let res = await sendEmail.run(
+          "New Friend Requests pending for " + email,
+          "Ther eare so many frined reuests pending, please login to DevTinder.in and accept or reject the reqyests."
+        );
+        console.log(res);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
